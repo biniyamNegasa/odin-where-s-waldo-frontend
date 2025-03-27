eb528a539973a885dd2074847e551b6d5dd53a8f
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-03-27 12:48:18 +0300

    feat: setup images

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const gameImages = [
-  { id: 1, title: "waldo", url: "../assets/waldo.jpg" },
-  { id: 2, title: "waldo", url: "../assets/waldo.jpg" },
-  { id: 3, title: "waldo", url: "../assets/waldo.jpg" },
-  { id: 4, title: "waldo", url: "../assets/waldo.jpg" },
-  { id: 5, title: "waldo", url: "../assets/waldo.jpg" },
+  { id: 1, title: "Ice Skating Theme", url: "/src/assets/ice.jpg" },
+  { id: 2, title: "Olympic Theme", url: "/src/assets/dome.jpg" },
+  { id: 3, title: "Red Carpet Theme", url: "/src/assets/red_carpet.jpg" },
+  { id: 4, title: "Beach Theme", url: "/src/assets/beach.jpg" },
+  { id: 5, title: "Space Theme", url: "/src/assets/space.jpg" },
 ];
